69506246ff7d20198af52ec0df43055b2bbf8517
t-bytes-2062 <tj2062@srmist.edu.in>
2026-02-23 08:38:30 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ *
+ * This class prints OOPS to the console.
+ *
+ * @author Tanish Joshi
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
+    
